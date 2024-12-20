9f6f2bfabd795387455c9e836d758324c2277459
Jida Li <77774296+jidalii@users.noreply.github.com>
2024-12-20 16:02:12 -0500

    feat: goSync

--- goSync/main.go
+++ goSync/main.go
@@ -1,9 +1,12 @@
 package main
 
 import (
+	"bytes"
+	"context"
 	"fmt"
 	"math/rand"
 	"sync"
+	"sync/atomic"
 	"time"
 )
 
@@ -174,68 +177,332 @@ func BankBalanceBenchmark() {
 }
 
 func BankBalance(rOp int, wOp int, useRWMutex bool) time.Duration {
-    // fmt.Println("********** BankBalance **********")
-    balance := 1000
-    var mutex sync.Locker
-    var rwMutex sync.RWMutex
-    var simpleMutex sync.Mutex
+	// fmt.Println("********** BankBalance **********")
+	balance := 1000
+	var mutex sync.Locker
+	var rwMutex sync.RWMutex
+	var simpleMutex sync.Mutex
 
-    if useRWMutex {
-        mutex = &rwMutex
-    } else {
-        mutex = &simpleMutex
-    }
+	if useRWMutex {
+		mutex = &rwMutex
+	} else {
+		mutex = &simpleMutex
+	}
 
-    wg := sync.WaitGroup{}
-    ch := make(chan int, rOp+wOp)
-    defer close(ch)
+	wg := sync.WaitGroup{}
+	ch := make(chan int, rOp+wOp)
+	defer close(ch)
 
-    updateBalance := func(mutex sync.Locker, ch chan int) {
-        randOp := func() int {
-            if rand.Intn(2) == 0 {
-                return 1
-            } else {
-                return -1
-            }
-        }
-        defer wg.Done()
-        mutex.Lock()
-        amount := randOp() * rand.Intn(balance)
-        balance += amount
-        mutex.Unlock()
-        ch <- amount
-    }
+	updateBalance := func(mutex sync.Locker, ch chan int) {
+		randOp := func() int {
+			if rand.Intn(2) == 0 {
+				return 1
+			} else {
+				return -1
+			}
+		}
+		defer wg.Done()
+		mutex.Lock()
+		amount := randOp() * rand.Intn(balance)
+		balance += amount
+		mutex.Unlock()
+		ch <- amount
+	}
 
-    readBalance := func(mutex sync.Locker, ch chan int) {
-        defer wg.Done()
-        if useRWMutex {
-            rwMutex.RLock()
-        } else {
-            mutex.Lock()
-        }
-        ch <- balance
-        if useRWMutex {
-            rwMutex.RUnlock()
-        } else {
-            mutex.Unlock()
-        }
-    }
+	readBalance := func(mutex sync.Locker, ch chan int) {
+		defer wg.Done()
+		if useRWMutex {
+			rwMutex.RLock()
+		} else {
+			mutex.Lock()
+		}
+		ch <- balance
+		if useRWMutex {
+			rwMutex.RUnlock()
+		} else {
+			mutex.Unlock()
+		}
+	}
 
-    startTime := time.Now()
+	startTime := time.Now()
 
-    for i := 0; i < rOp; i++ {
-        wg.Add(1)
-        go readBalance(mutex, ch)
-    }
+	for i := 0; i < rOp; i++ {
+		wg.Add(1)
+		go readBalance(mutex, ch)
+	}
 
-    for i := 0; i < wOp; i++ {
-        wg.Add(1)
-        go updateBalance(mutex, ch)
-    }
+	for i := 0; i < wOp; i++ {
+		wg.Add(1)
+		go updateBalance(mutex, ch)
+	}
 
-    wg.Wait()
+	wg.Wait()
 	totalTime := time.Since(startTime)
 	return totalTime
+}
+
+//*´:°•.°+.*•´.*:˚.°*.˚•´.°:°•.°•.*•´.*:˚.°*.˚•´.°:°•.°+.*•´.*:*//
+//*                         WAITGROUP                          *//
+//*.•°:°.´+˚.*°.˚:*.´•*.+°.•°:´*.´•*.•°.•°:°.´:•˚°.*°.˚:*.´+°.•*//
+
+func FindPrimeNumbersInRange(start, end int) {
+	// Create a WaitGroup
+	var wg sync.WaitGroup
+
+	// Add the number of goroutines to the WaitGroup
+	wg.Add(end - start + 1)
+
+	// Create a goroutine for each number
+	for i := start; i <= end; i++ {
+		go IsPrime(i, &wg)
+	}
+
+	// Block here until all goroutines to finish
+	wg.Wait()
+}
+
+func IsPrime(n int, wg *sync.WaitGroup) bool {
+	// Defer Done to notify the WaitGroup when the task done
+	defer wg.Done()
+
+	if n < 2 {
+		return false
+	}
+	for i := 2; i*i <= n; i++ {
+		if n%i == 0 {
+			return false
+		}
+	}
+	fmt.Println("Prime number:", n)
+	return true
+}
+
+//*´:°•.°+.*•´.*:˚.°*.˚•´.°:°•.°•.*•´.*:˚.°*.˚•´.°:°•.°+.*•´.*:*//
+//*                           ONCE                             *//
+//*.•°:°.´+˚.*°.˚:*.´•*.+°.•°:´*.´•*.•°.•°:°.´:•˚°.*°.˚:*.´+°.•*//
+
+type DB struct{}
+
+var db *DB
+var once sync.Once
+
+func GetDB() *DB {
+	once.Do(func() {
+		db = &DB{}
+		fmt.Println("db instance created.")
+	})
+	return db
+}
+
+func (d *DB) Query() {
+	fmt.Println("Querying the db.")
+}
+
+func SingletonOnce() {
+	var wg sync.WaitGroup
+	wg.Add(10)
+	for i := 0; i < 10; i++ {
+		go func() {
+			defer wg.Done()
+			GetDB().Query()
+		}()
+	}
+	wg.Wait()
+}
+
+//*´:°•.°+.*•´.*:˚.°*.˚•´.°:°•.°•.*•´.*:˚.°*.˚•´.°:°•.°+.*•´.*:*//
+//*                           POOL                             *//
+//*.•°:°.´+˚.*°.˚:*.´•*.+°.•°:´*.´•*.•°.•°:°.´:•˚°.*°.˚:*.´+°.•*//
+
+func PoolExample(workNum int) {
+	// Counter for new buffer creations
+	created := atomic.Int32{}
+
+	bufferPool := sync.Pool{
+		// New optionally specifies a function to generate
+		// a value when Get would otherwise return nil.
+		New: func() interface{} {
+			created.Add(1)
+			return new(bytes.Buffer)
+		},
+	}
+
+	worker := func(id int, wg *sync.WaitGroup) {
+		defer wg.Done()
+
+		// Get buffer from pool
+		buf := bufferPool.Get().(*bytes.Buffer)
+		defer bufferPool.Put(buf)
+
+		// Actually use the buffer
+		buf.Reset()
+		fmt.Fprintf(buf, "Worker %d using buffer\n", id)
+		fmt.Print(buf.String())
+	}
+
+	wg := new(sync.WaitGroup)
+	for i := 0; i < workNum; i++ {
+		wg.Add(1)
+		// Simulate request pattern: between 0ms - 10ms
+		time.Sleep(time.Duration(rand.Intn(10)) * time.Millisecond)
+		go worker(i, wg)
+	}
+	wg.Wait()
+
+	fmt.Printf("Created %d buffers\n", created.Load())
+}
+
+//*´:°•.°+.*•´.*:˚.°*.˚•´.°:°•.°•.*•´.*:˚.°*.˚•´.°:°•.°+.*•´.*:*//
+//*                           Cond                             *//
+//*.•°:°.´+˚.*°.˚:*.´•*.+°.•°:´*.´•*.•°.•°:°.´:•˚°.*°.˚:*.´+°.•*//
+
+// ********** Producer-Consumer with Cond **********
+
+func producer(ctx context.Context, id int, c *sync.Cond) error {
+	for {
+		select {
+		case <-ctx.Done():
+			return ctx.Err()
+		default:
+			c.L.Lock()
+			// Produce something...
+			fmt.Printf("%s- Producer %d: producing...\n", time.Now().Format("15:04:05.999"), id)
+			time.Sleep(time.Duration(rand.Intn(100)) * time.Millisecond)
+			c.Signal()
+			c.L.Unlock()
+			time.Sleep(1 * time.Second)
+		}
+	}
+}
+
+func consumer(ctx context.Context, id int, c *sync.Cond) error {
+	for {
+		select {
+		case <-ctx.Done():
+			return ctx.Err()
+		default:
+			c.L.Lock()
+			c.Wait()
+			// Consume something...
+			fmt.Printf("%s- Consumer %d: consuming...\n", time.Now().Format("15:04:05.999"), id)
+			time.Sleep(time.Duration(rand.Intn(100)) * time.Millisecond)
+			c.L.Unlock()
+		}
+	}
+}
+
+func ProducerSingleConsumer() {
+	mutex := sync.Mutex{}
+	c := sync.NewCond(&mutex)
+	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
+	defer cancel()
+
+	go producer(ctx, 1, c)
+	go consumer(ctx, 1, c)
+	// go consumer(ctx, 2, c)
+	fmt.Println("Producer and consumer started.")
+	time.Sleep(5 * time.Second)
+	fmt.Println("Producer and consumer ended.")
+}
+
+func ProducerMultiConsumers() {
+	mutex := sync.Mutex{}
+	c := sync.NewCond(&mutex)
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+
+	go producer(ctx, 1, c)
+	go consumer(ctx, 1, c)
+	go consumer(ctx, 2, c)
+	fmt.Println("Producer and consumer started.")
+	time.Sleep(5 * time.Second)
+	fmt.Println("Producer and consumer ended.")
+}
+
+// ********** Pub/Sub with Cond **********
+
+func publisher(ctx context.Context, id int, c *sync.Cond) error {
+	for {
+		select {
+		case <-ctx.Done():
+			return ctx.Err()
+		default:
+			c.L.Lock()
+			// Produce something...
+			time.Sleep(time.Duration(rand.Intn(200)) * time.Millisecond)
+			fmt.Printf("%s- Publisher %d: generating...\n", time.Now().Format("15:04:05.999"), id)
+			c.Broadcast()
+			c.L.Unlock()
+			time.Sleep(1 * time.Second)
+		}
+	}
+}
+
+func subscriber(ctx context.Context, id int, c *sync.Cond) error {
+	for {
+		select {
+		case <-ctx.Done():
+			return ctx.Err()
+		default:
+			c.L.Lock()
+			c.Wait()
+			// Consume something...
+			fmt.Printf("%s- Subscriber %d: reading...\n", time.Now().Format("15:04:05.999"), id)
+			c.L.Unlock()
+		}
+	}
+}
+
+func PubMultiSub() {
+	mutex := sync.Mutex{}
+	c := sync.NewCond(&mutex)
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+
+	go publisher(ctx, 1, c)
+	go subscriber(ctx, 1, c)
+	go subscriber(ctx, 2, c)
+	go subscriber(ctx, 3, c)
+	fmt.Println("Pub/Sub started.")
+	time.Sleep(5 * time.Second)
+	fmt.Println("Pub/Sub ended.")
+}
+
+func CondExampleWithPizzaRace() {
+	mutex := sync.Mutex{}
+	c := sync.NewCond(&mutex)
+	wg := sync.WaitGroup{}
+	pizzaRemaining := 10
+	person := func(id int, c *sync.Cond, wg *sync.WaitGroup) {
+		defer wg.Done()
+		c.L.Lock()
+		c.Wait() // Waits for broadcast
+		c.L.Unlock()
+		for {
+			c.L.Lock()
+			if pizzaRemaining <= 0 {
+				c.L.Unlock()
+				return
+			}
+			pizzaRemaining--
+			fmt.Printf("Person %d got the pizza. Remaining: %d - %s\n",
+				id, pizzaRemaining, time.Now().Format("15:04:05.999"))
+			c.L.Unlock()
+
+			// Simulate eating time
+			time.Sleep(100 * time.Millisecond)
+		}
+	}
+
+	for i := 0; i < 3; i++ {
+		wg.Add(1)
+		go person(i, c, &wg)
+	}
+	time.Sleep(100 * time.Millisecond)
+
+	c.Broadcast()
+
+	wg.Wait()
 }
 
 func main() {
@@ -245,7 +512,21 @@ func main() {
 	// BankMutex()
 
 	// RWMutex
-	SimpleRWMutex()
+	// SimpleRWMutex()
+	// BankBalanceBenchmark()
 
-	BankBalanceBenchmark()
+	// WaitGroup
+	// FindPrimeNumbersInRange(0, 100)
+
+	// Once
+	// SingletonOnce()
+
+	// Pool
+	// PoolExample(1000)
+
+	// Cond
+	ProducerSingleConsumer()
+	ProducerMultiConsumers()
+	PubMultiSub()
+	CondExampleWithPizzaRace()
 }
